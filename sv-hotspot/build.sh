83c7c33e396b05f077fc47aa0010676374470861
eteleeb <eteleeb@hotmail.com>
2020-03-13 12:19:37 -0500

    update meta.yaml

--- sv-hotspot/build.sh
+++ sv-hotspot/build.sh
@@ -4,7 +4,8 @@ mkdir -p $PREFIX/bin
 cp -v src/*.r $PREFIX/bin
 cp -v src/*.sh $PREFIX/bin
 cp -v src/*.pl $PREFIX/bin
-cp -r -v src/annotations $PREFIX/bin
+cp -r -v src/annotations $PREFIX
+cp -r -v test_data $PREFIX
 
 chmod -R 0755 $PREFIX/bin
 
